af6bb8807cebe4f0f5d1764975a3523edf18d808
Junior <claudio.junior14@hotmail.com>
2019-11-29 09:59:58 -0300

    Backend backup method adjust

--- backend/src/controllers/Backup.js
+++ backend/src/controllers/Backup.js
@@ -3,9 +3,44 @@ const sql = require('mssql');
 const Backup = {
   async inserir(req, res) {
     try {
-      const response = await req.body.params;
-      return await res.json(response);
-      return await res.json(req.body);
+      const body = await req.body;
+
+      await sql.connect('mssql://sa:141018@localhost/master');
+      const result = await sql.query(
+        `INSERT INTO dbo.backupForm (
+          quem,
+          empresa,
+          local,
+          data,
+          funcionarios,
+          horas,
+          horaExtra,
+          viagem,
+          veiculos,
+          terceiros,
+          terceirosFunc,
+          crea
+        )
+        values (
+          '${body.quem}',
+          '${body.empresa}',
+          '${body.local}',
+          '${body.data}',
+          '${body.funcionarios}',
+          ${body.horas},
+          '${body.horasExtras}',
+          '${body.viagem}',
+          '${body.veiculos}',
+          '${body.terceiros}',
+          '${body.terceirosFunc}',
+          ${body.crea}
+          )`
+      );
+      console.log(body);
+      return res.json({
+        body: body,
+        status: 200
+      });
     } catch (error) {
       console.log(error);
     }
