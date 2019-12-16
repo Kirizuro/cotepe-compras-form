15e8f09b74f2f8e0ed6033036d855caa24f6f0ee
Junior <claudio.junior14@hotmail.com>
2019-12-16 16:41:26 -0300

    Backend adaption for enterprise DB

--- backend/src/controllers/Querys.js
+++ backend/src/controllers/Querys.js
@@ -29,8 +29,17 @@ const Querys = {
 
   async parceiros(req, res) {
     try {
-      await sql.connect('mssql://sa:141018@localhost/master');
-      const result = await sql.query(`select * from dbo.parceiros`);
+      await sql.connect('mssql://claudio:Io985641cl@@SRVSAP/COTEPE_Producao');
+      //await sql.connect('mssql://sa:141018@localhost/master');
+      //const result = await sql.query(`select * from dbo.parceiros`);
+      const result = await sql.query(
+        `select * from dbo.COTEPE_PARCEIRODENEGOCIO_CNPJ order by Nome ASC`
+      );
+
+      /*const result = await sql.query(`select * from dbo.Acompanhamento_Parceiros_de_Negocio_Endereco as adress,
+                                        dbo.Acompanhamento_Parceiros_de_Negocio_CNPJ_EI as cnpj
+                                        where adress.CardCode = cnpj.CardCode order by CardName ASC`);
+                                        */
       return res.json({
         result,
         status: 200
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -4,7 +4,6 @@ const routes = express.Router();
 
 const Querys = require('./controllers/Querys');
 const Backup = require('./controllers/Backup');
-const Login = require('./controllers/Login');
 
 routes.get('/vendas', Querys.vendas);
 routes.get('/funcionarios', Querys.funcionarios);
@@ -12,8 +11,5 @@ routes.get('/parceiros', Querys.parceiros);
 
 routes.get('/backupShow', Backup.mostrar);
 routes.post('/backup', Backup.inserir);
-
-routes.post('/register', Login.inserir);
-routes.post('/login', Login.login);
 
 module.exports = routes;
